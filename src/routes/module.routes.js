7fabeb25cef7dfced34e90b13e93c3b205bf3f54
DmnkWnsch <wuensch_dominik@t-online.de>
2023-11-20 18:34:54 +0100

    Update module route to contain more information
    Route /:moduleId now includes the name and credits of the module to reduce database requests in the final application

--- src/routes/module.routes.js
+++ src/routes/module.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import moduleController from "../controllers/module.controller.js";
+import examController from "../controllers/exam.controller.js";
 
 const moduleRouter = Router();
 
 moduleRouter.get("/", moduleController.getAllModules);
 moduleRouter.get("/:moduleId", moduleController.getModule);
+moduleRouter.get("/:moduleId/exams", examController.getExamsForModule);
 
 export default moduleRouter;
--- src/services/module.service.js
+++ src/services/module.service.js
@@ -3,7 +3,7 @@ import database from "../database/database.js";
 const getModulesForCourse = async (courseId) => {
   // Return modules for course x
   const rows = await database.query(
-    "SELECT * FROM course_module_types WHERE `course_id` = ?",
+    "SELECT course_id, module_id, type, name, credits, planned_semester FROM course_module_types JOIN modules ON course_module_types.module_id=modules.id WHERE `course_id` = ? ORDER BY module_id",
     [courseId]
   );
 
