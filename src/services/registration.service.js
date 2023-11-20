76abb44422594f23f06dc14af9c4041b594fa04f
DmnkWnsch <wuensch_dominik@t-online.de>
2023-11-20 18:55:05 +0100

    Update registration for member to include more information

--- src/services/registration.service.js
+++ src/services/registration.service.js
@@ -2,7 +2,7 @@ import database from "../database/database.js";
 
 const getRegistrationsForMember = async (memberId) => {
   const result = await database.query(
-    "SELECT * FROM exam_registrations JOIN exam_plan ON exam_plan.uid=exam_registrations.exam_plan_id WHERE member_id = ?",
+    "SELECT status, exam_id, date, term, module_id, type FROM exam_registrations JOIN exam_plan ON exam_plan.uid=exam_registrations.exam_plan_id JOIN exams ON exam_plan.exam_id=exams.id WHERE member_id = ?",
     [memberId]
   );
 
