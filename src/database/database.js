96dd4a6e08df62cae07191e48916dacba02e7a67
DmnkWnsch <wuensch_dominik@t-online.de>
2024-01-04 14:33:20 +0100

    Fix too many connections error

--- src/database/database.js
+++ src/database/database.js
@@ -10,9 +10,24 @@ const config = {
   database: "to_praktikum",
 };
 
+const pool = mysql.createPool({
+  host: "85.209.49.177",
+  user: "toprak",
+  //password: process.env.DB_PW,
+  password: "7BfPKAmLaSKHeRnY76lF",
+  database: "to_praktikum",
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
+
 async function query(sql, params) {
-  const connection = await mysql.createConnection(config);
-  const [results] = await connection.execute(sql, params);
+  //const connection = await mysql.createConnection(config);
+  const [results] = await pool.execute(sql, params);
 
   return results;
 }
