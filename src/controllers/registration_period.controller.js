f3dc9c2b8899bd0de99df537e453864da15127b7
DmnkWnsch <wuensch_dominik@t-online.de>
2024-01-16 19:05:07 +0100

    Add functions to update and delete register periods

--- src/controllers/registration_period.controller.js
+++ src/controllers/registration_period.controller.js
@@ -66,8 +66,68 @@ const addRegistrationPeriod = async (req, res) => {
   }
 };
 
+/**
+ * Deletes a given registration period
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param req.params.periodId - the id of the period to delete
+ */
+const deleteRegistrationPeriod = async (req, res) => {
+  const periodId = req.params.periodId;
+
+  try {
+    const result = await registrationPeriodService.deleteRegistrationPeriod(
+      periodId
+    );
+    res.status(200).send({ deleted: periodId });
+  } catch (error) {
+    responseUtil.sendDefaultErrorResponse(res, error);
+  }
+};
+
+/**
+ * Updates a given registration period
+ * @function
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param req.params.periodId - the id of the period to update
+ * @param req.body - the payload for the period
+ */
+const updateRegistrationPeriod = async (req, res) => {
+  const periodId = req.params.periodId;
+  const payload = req.body;
+  const expectedParams = ["start_date", "end_date"];
+
+  if (!periodId) {
+    responseUtil.sendMissingRouteParamsResponse(res, ["periodId"]);
+    return;
+  }
+
+  if (!paramsUtil.allParametersSet(payload, expectedParams)) {
+    responseUtil.sendMissingParamsResponse(res, expectedParams);
+    return;
+  }
+
+  const periodData = {
+    id: periodId,
+    start_date: payload.start_date,
+    end_date: payload.end_date,
+  };
+
+  try {
+    const updatedPeriod =
+      await registrationPeriodService.updateRegistrationPeriod(periodData);
+    res.status(200).send({ data: updatedPeriod });
+  } catch (error) {
+    responseUtil.sendDefaultErrorResponse(res, error);
+  }
+};
+
 export default {
   getRegistrationPeriods,
   getRegistrationPeriod,
   addRegistrationPeriod,
+  deleteRegistrationPeriod,
+  updateRegistrationPeriod,
 };
